54385374ecb7d11fbca1f27fce43f2bba25c57e1
ItsRichardScull <totallynotrichardscull@gmail.com>
2023-12-19 13:34:27 +0200

    chore: Add comment for USELOGGER

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 require("dotenv").config();
 import "./utils/checkEnv";
 
+// Used for debugging purposes,
+// if you are not a developer it's better to leave it as false
 export const USELOGGER = true;
 
 import "./database/main";
