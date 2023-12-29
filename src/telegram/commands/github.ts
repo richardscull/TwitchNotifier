5837705575ed78c474e777f87c7429892bdedd4f
ItsRichardScull <totallynotrichardscull@gmail.com>
2023-12-29 05:59:39 +0200

    feat: Add /github command

---
+++ src/telegram/commands/github.ts
@@ -0,0 +1,24 @@
+import { Attributes, TelegramClient } from "../client";
+
+module.exports = {
+  regex: /^\/github$/,
+  requireToken: true,
+  execute(attr: Attributes, localizationFile: any) {
+    const { ctx, msg, userId } = attr;
+
+    const button = {
+      text: localizationFile["commands"]["github"]["repository"],
+      url: localizationFile["commands"]["github"]["github_link"],
+    };
+
+    ctx.Reply(msg, {
+      text: localizationFile["commands"]["github"]["text"],
+      image: "./assets/random_girl.png",
+      options: {
+        reply_markup: {
+          inline_keyboard: [[button]],
+        },
+      },
+    });
+  },
+};
