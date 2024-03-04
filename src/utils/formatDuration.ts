248182e66b3b69fff1f80e390b661519d3ea595a
ItsRichardScull <totallynotrichardscull@gmail.com>
2024-03-04 22:59:11 +0200

    fix: 0 Seconds bug for getDuration

--- src/utils/formatDuration.ts
+++ src/utils/formatDuration.ts
@@ -46,7 +46,6 @@ const languages = {
 };
 
 export function getDuration(seconds: number, language: Language) {
-  seconds = Number(seconds);
   const d = Math.floor(seconds / (3600 * 24));
   const h = Math.floor((seconds % (3600 * 24)) / 3600);
   const m = Math.floor((seconds % 3600) / 60);
@@ -58,7 +57,7 @@ export function getDuration(seconds: number, language: Language) {
   for (let i = 0; i < values.length; i++) {
     const value = eval(values[i]);
 
-    if (value > 0) {
+    if (value > 0 || (seconds == 0 && i == 3)) {
       const word = Pluralize(value, "", languages[language][values[i]]);
       return_value += `${value} ${word} `;
     }
